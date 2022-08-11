4e46d71c478f61b2d9ef9643ca313875cee2d858
Stas Vinnikov <stashek666@gmail.com>
2022-08-11 20:14:41 +0300

    3е задание выполнено

--- 21_Tuples/03_function/main.py
+++ 21_Tuples/03_function/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+import random
+
+
+def slicer(tuple_, num):
+
+    if num not in tuple_:
+        return ()
+    if tuple_.count(num) == 1:
+        return tuple_[tuple_.index(num):]
+    return tuple_[tuple_.index(num)
+                  :tuple_.index(num, tuple_.index(num) + 1) + 1
+                  ]
+
+
+random_list = [random.randint(1, 10) for _ in range(10)]
+
+random_tuple = tuple(random_list)
+number = int(input('Введите число: '))
+
+print(random_tuple, number)
+print('\n', slicer(random_tuple, number), ', ', number)
+
+
