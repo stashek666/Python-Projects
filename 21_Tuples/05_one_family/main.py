56892a5ec79ead21ed8df223f874667ba2b08110
Stas Vinnikov <stashek666@gmail.com>
2022-08-11 20:53:36 +0300

    5е задание выполнено

--- 21_Tuples/05_one_family/main.py
+++ 21_Tuples/05_one_family/main.py
@@ -1,1 +1,37 @@
-# TODO здесь писать код
+family_dict = {
+
+    ('Сидоров', 'Никита'): 35,
+
+    ('Сидорова', 'Алина'): 34,
+
+    ('Сидоров', 'Павел'): 10,
+
+    ('Иванов', 'Максим'): 45,
+
+    ('Иванова', 'Мария'): 41
+
+                }
+
+
+surname = input("Введите фамилию: ").lower()
+
+if surname not in family_dict:
+    print('Поиск не дал результатов.')
+
+elif surname[-1] == 'а':
+
+    surname_man = surname[:-1]
+
+    for i_name, i_age in family_dict.items():
+
+        if i_name[0].lower() == surname or i_name[0].lower() == surname_man:
+            print(i_name[0], i_name[1], i_age)
+
+else:
+
+    surname_woman = surname + 'а'
+
+    for i_name, i_age in family_dict.items():
+
+        if i_name[0].lower() == surname or i_name[0].lower() == surname_woman:
+            print(i_name[0], i_name[1], i_age)
