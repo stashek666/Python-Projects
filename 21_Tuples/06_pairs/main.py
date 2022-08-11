8af3a9178f786c0dc96f0c84eed6c34ec6c66754
Stas Vinnikov <stashek666@gmail.com>
2022-08-11 22:37:04 +0300

    6е задание выполнено

--- 21_Tuples/06_pairs/main.py
+++ 21_Tuples/06_pairs/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+import random
+
+original_list = [random.randint(0, 9) for _ in range(10)]
+print('Оригинальный список: ', original_list)
+
+print('\n1й способ:')
+
+zip_list = zip(original_list[::2], original_list[1::2])
+new_list = [pair for pair in zip_list]
+
+print('Новый список:', new_list)
+
+print('\n2й способ:')
+
+new_list_2 = [(value, original_list[index * 2 + 1])
+               for index, value in enumerate(original_list[::2])
+               ]
+
+print('Новый список v2:', new_list_2)
