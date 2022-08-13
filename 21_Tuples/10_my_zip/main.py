039598438f5b40f5cf9c9cfe7574ed2ef2034962
Stas Vinnikov <stashek666@gmail.com>
2022-08-13 18:49:18 +0300

    10е задание выполнено

--- 21_Tuples/10_my_zip/main.py
+++ 21_Tuples/10_my_zip/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def shortest_len(string, tpl):
+    return min(len(string), len(tpl))
+
+sym_string = 'abc'
+nums_tpl = (10, 20, 30, 40)
+
+pairs = ((sym_string[i_elem], nums_tpl[i_elem])
+         for i_elem in range(shortest_len(sym_string, nums_tpl)))
+print(pairs)
+
+for i in pairs:
+    print(i)
+
+print(zip(sym_string, nums_tpl))
