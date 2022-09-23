9ada0ab5306bbb3806396c4715a34d600bc11276
Stas Vinnikov <stashek666@gmail.com>
2022-09-23 16:32:46 +0300

    2е задание решено

--- 23_Files/02_zen_of_python/main.py
+++ 23_Files/02_zen_of_python/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+zen_file = open('zen.txt', 'r')
+
+lst1 = zen_file.read().split('\n')
+lst2 = lst1[::-1]
+
+for i_string in lst2:
+    print(i_string)
+
+zen_file.close()
+
