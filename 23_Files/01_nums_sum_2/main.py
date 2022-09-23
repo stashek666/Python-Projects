de4420da7c1d9c1f5bbcbcd0319b8ae1adfa9aa8
Stas Vinnikov <stashek666@gmail.com>
2022-09-23 16:24:17 +0300

    1е задание решено

--- 23_Files/01_nums_sum_2/main.py
+++ 23_Files/01_nums_sum_2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+input_data_file = open('numbers.txt', 'r', encoding='utf-8')
+
+data_list = input_data_file.read().split()
+summa = 0
+
+for num in data_list:
+    summa += int(num)
+input_data_file.close()
+
+output_data_file = open('answer.txt', 'w', encoding='utf-8')
+output_data_file.write(str(summa))
+output_data_file.close()
