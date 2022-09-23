2d4878f42a9e88118f159b5f4a1241e79d037d2e
Stas Vinnikov <stashek666@gmail.com>
2022-09-23 19:25:38 +0300

    3е задание решено

--- 23_Files/03_zen_of_python_2/main.py
+++ 23_Files/03_zen_of_python_2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import string
+
+file = open(r'C:\python-ds\23_Files\02_zen_of_python\zen.txt')
+
+file_read = file.read()
+file_words = file_read.split()
+file_rls = file_read.split('\n')
+chars = [i for i in file_read if i in string.ascii_letters]
+
+print(f'Количество букв в файле: {len(chars)}')
+print(f'Количество слов в файле: {len(file_words)}')
+print(f'Количество строк в файле: {len(file_rls)}')
+print(f'Наиболее редкая буква: {min(chars, key=lambda x: file_read.count(x))}')
+file.close()
