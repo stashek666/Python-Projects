648740692438ff90841aaf2bfdc935f8e97da8b3
Stas Vinnikov <stashek666@gmail.com>
2022-09-24 16:05:45 +0300

    5е задание решено

--- 23_Files/05_save/main.py
+++ 23_Files/05_save/main.py
@@ -1,1 +1,46 @@
-# TODO здесь писать код
+import os
+
+
+def save_files(string):
+
+    way = input('\nКуда хотите сохранить документ? '
+                'Введите последовательность папок (через пробел): ')
+    file_name = input('\nВведите имя файла: ')
+
+    r_path = way.replace(" ", "/")
+    path = 'C:/' + r_path
+
+    if os.path.exists(path):
+        real_path = os.path.realpath(os.path.join(path, file_name))
+        check_file = os.path.exists(real_path)
+
+        if check_file:
+
+            print('\nФайл с таким именем уже существует!')
+            ans_q = input('Вы действительно хотите '
+                          'перезаписать файл? (yes/no): ').lower()
+            if ans_q == 'yes':
+                file = open(real_path, 'w')
+                file.write(string)
+                print('Файл успешно перезаписан!')
+                file.close()
+            else:
+                print('Файл не перезаписан')
+        else:
+            file = open(real_path, 'w')
+            file.write(string)
+            print('Файл успешно сохранён!')
+            file.close()
+    else:
+        print('Такого пути нет')
+
+
+text = input('Введите строку: ')
+save_files(text)
+
+# test_path = 'python-ds 23_Files 05_save'
+# test_name = 'text.txt'
+
+
+
+
--- 23_Files/06_paranoia/main.py
+++ 23_Files/06_paranoia/main.py
@@ -1,1 +1,34 @@
-# TODO здесь писать код
+def cipher(text, k):
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz' \
+              'abcdefghijklmnopqrstuvwxyz' \
+              'abcdefghijklmnopqrstuvwxyz' \
+
+    result = ''
+
+    for i_sym in text:
+        sym_index = alphabet.find(i_sym)
+        new_sym_index = sym_index + k
+        if i_sym in alphabet:
+            result += alphabet[new_sym_index]
+        else:
+            result += i_sym
+    return result
+
+
+input_file = open(r'C:\python-ds\23_Files\06_paranoia\text.txt')
+output_file = open(r'C:\python-ds\23_Files\06_paranoia\cipher_text.txt', 'a')
+shift = 0
+
+for i in input_file:
+    shift += 1
+    res = cipher(i.lower(), shift)
+    output_file.write(res.title())
+
+input_file.close()
+output_file.close()
+
+
+
+
+
