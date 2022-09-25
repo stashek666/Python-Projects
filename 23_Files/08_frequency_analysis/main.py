3418403028241adb7ab77776ca9a74bb5c90a1b1
Stas Vinnikov <stashek666@gmail.com>
2022-09-25 23:45:13 +0300

    8е задание решено

--- 23_Files/08_frequency_analysis/main.py
+++ 23_Files/08_frequency_analysis/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+string = open("text.txt", "r").read().lower()
+
+sym_dict = {}
+count = 0
+
+for i_sym in string:
+
+   if ord('a') <= ord(i_sym) <= ord('z'):
+       find_sym = sym_dict.get(i_sym, 0)
+       sym_dict[i_sym] = find_sym + 1
+       count += 1
+
+frequency_list = [(i_key, "{:5.3f}".format(sym_dict[i_key]/count))
+                  for i_key in sym_dict.keys()]
+
+frequency_list.sort(key=lambda x: x[0])
+frequency_list.sort(key=lambda x: x[1], reverse=True)
+
+result = "\n".join([i_elem[0] + " " + i_elem[1]
+                    for i_elem in frequency_list])
+open("output.txt", "w").write(result)
