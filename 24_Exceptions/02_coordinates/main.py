2f2b73102c1c4039f9acae83fb39790681a087a5
Stas Vinnikov <stashek666@gmail.com>
2022-09-28 19:58:19 +0300

    2е задание решено

--- 24_Exceptions/02_coordinates/main.py
+++ 24_Exceptions/02_coordinates/main.py
@@ -14,26 +14,22 @@ def f2(x, y):
 
 
 try:
-    file = open('coordinates.txt', 'r')
-    for line in file:
-        nums_list = line.split()
-        res1 = f(int(nums_list[0]), int(nums_list[1]))
-        try:
+
+    with open('coordinates.txt', 'r') as file:
+        for line in file:
+            nums_list = line.split()
+            res1 = f(int(nums_list[0]), int(nums_list[1]))
             res2 = f2(int(nums_list[0]), int(nums_list[1]))
-            try:
-                number = random.randint(0, 100)
-                file_2 = open('result.txt', 'w')
-                my_list = sorted([res1, res2, number])
-                file_2.write(' '.join(my_list))
-            except Exception:
-                print("Что-то пошло не так")
-        except Exception:
-            print("Что-то пошло не так со второй функцией")
-        finally:
-            file.close()
+            file_2 = open('result.txt', 'w')
+            number = random.randint(0, 100)
+            my_list = sorted([res1, res2, number])
+            file_2.write(' '.join(str(my_list)))
             file_2.close()
+
 except Exception:
-    print("Что-то пошло не так с первой функцией")
-
-
-# TODO отредактировать и исправить программу
+    print("Что-то пошло не так")
+else:
+    print('Программа выполнилась без ошибок.')
+finally:
+    print('coordinates.txt close:', file.closed)
+    print('result.txt close:', file_2.closed)
