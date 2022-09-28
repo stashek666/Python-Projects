1e9e38bd2326b03b585ca43b721eb4f6ce8610f0
Stas Vinnikov <stashek666@gmail.com>
2022-09-28 21:00:11 +0300

    3е задание решено

--- 24_Exceptions/03_lucky_number/main.py
+++ 24_Exceptions/03_lucky_number/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+import random
+
+errors = [
+    'Не повезло',
+    'Повезло но не сильно',
+    'Вас постигла неудача!'
+]
+
+try:
+
+    with open('result.txt', 'a') as result:
+        num_sum = 0
+        while num_sum < 777:
+            num = int(input('Введите число: '))
+            num_sum += num
+            random_num = random.randint(1, 13)
+            if random_num == 13:
+                print('Сумма чисел: ', num_sum)
+                raise Exception('Выпало число 13')
+            else:
+                result.write(str(num))
+                result.write('\n')
+
+except Exception:
+    raise Exception(random.choice(errors))
+
+else:
+    print('Все прошло без ошибок.')
+finally:
+    print(result.closed)
+
+
