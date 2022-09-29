56e0477b3234de12dd9d736adce1f7de7469f547
Stas Vinnikov <stashek666@gmail.com>
2022-09-29 19:08:36 +0300

    7е задание решено

--- 24_Exceptions/07_text_calc_2/main.py
+++ 24_Exceptions/07_text_calc_2/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+with open('calc.txt', 'r') as file:
+
+    print('Содержимое файла calc.txt')
+    for i in file:
+        print(i)
+
+result = []
+
+with open('calc.txt', 'r') as file:
+
+    print('\nСодержимое консоли:')
+    for line in file.readlines():
+        try:
+            result.append(eval(line))
+        except SyntaxError:
+
+            if input('Обнаружена ошибка в строке:' +
+                     line + 'Хотите исправить?(yes/no): ') == 'yes':
+                line = input('Введите исправленную строку: ')
+                result.append(eval(line))
+
+print('\nСумма результатов:', sum(result))
