01e7cae7e53707c577dbb2c3c9a50c4d0194ea8f
Stas Vinnikov <stashek666@gmail.com>
2022-09-29 19:31:35 +0300

    6е задание решено

--- 24_Exceptions/06_chat/main.py
+++ 24_Exceptions/06_chat/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+import time
+
+user_name = input('Введите имя: ')
+
+while True:
+
+    print('Введите 1, чтобы увидеть историю сообщений,'
+          'введите 2, чтобы написать сообщение.')
+    command = input('Введите команду(1/2): ')
+    if command == '1':
+        try:
+            with open('chat.txt', 'r', encoding='utf-8') as file:
+                history_messages = file.readlines()
+                print(''.join(history_messages))
+        except FileNotFoundError:
+            print('История сообщений пуста!\n')
+    elif command == '2':
+        message = input('Введите сообщение: ')
+        with open('chat.txt', 'a', encoding='utf-8') as file:
+            file.write('{name}: {text}\n'.format(name=user_name, text=message))
+    else:
+        print('Неизвестная команда\n')
