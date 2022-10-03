0cc2393a3105df66ca36f6d37fd90d7db7341159
Stas Vinnikov <stashek666@gmail.com>
2022-10-04 00:34:40 +0300

    9е задание решено

--- 25_OOP/08_blackjack/main.py
+++ 25_OOP/08_blackjack/main.py
@@ -8,20 +8,14 @@ class Card(object):
         self.suit = suit
 
     def card_value(self):
-        """ Возращает количество очков которое дает карта """
 
         if self.rank in 'TJQK':
-            # По 10 за десятку, валета, даму и короля
             return 10
         else:
-            # Возвращает нужное число очков за любую другую карту
-            # Туз изначально дает одно очко.
             return 'A23456789'.index(self.rank)
-
 
     def get_rank(self):
         return self.rank
-
 
     def __str__(self):
         return '%s%s' % (self.rank, self.suit)
@@ -29,26 +23,19 @@ class Card(object):
 
 class Hand(object):
     def __init__(self, name):
-        # имя игрока
         self.name = name
-        # Изначально рука пустая
         self.cards = []
 
     def add_card(self, card):
-        """ Добавляет карту на руку """
         self.cards.append(card)
 
     def get_value(self):
-        """ Метод получения числа очков на руке """
         result = 0
-        # Количество тузов на руке.
         aces = 0
         for card in self.cards:
             result += card.card_value()
-            # Если на руке есть туз - увеличиваем количество тузов
             if card.get_rank() == 'A':
                 aces += 1
-        # Решаем считать тузы за 1 очко или за 11
         if result + aces * 10 <= 21:
             result += aces * 10
         return result
@@ -63,44 +50,33 @@ class Hand(object):
 
 class Deck(object):
     def __init__(self):
-        # ранги
         ranks = '23456789TJQKA'
-        # масти
         suits = 'БТЧП'
-        # генератор списков создающий колоду из 52 карт
         self.cards = [Card(r, s) for r in ranks for s in suits]
-        # перетасовываем колоду.
-        # Не забудьте импортировать функцию shuffle из модуля random
         random.shuffle(self.cards)
 
     def deal_card(self):
-        """ Функция сдачи карты """
         return self.cards.pop()
 
 
 def new_game():
-    # создаем колоду
+
     d = Deck()
-    # задаем "руки" для игрока и дилера
     player_hand = Hand(name)
     dealer_hand = Hand('Дилер')
-    # сдаем две карты игроку
     player_hand.add_card(d.deal_card())
     player_hand.add_card(d.deal_card())
-    # сдаем одну карту дилеру
     dealer_hand.add_card(d.deal_card())
     print(dealer_hand)
     print('=' * 20)
     print(player_hand)
-    # Флаг проверки необходимости продолжать игру
     in_game = True
-    # набирать карты игроку имеет смысл только если у него на руке меньше 21 очка
+
     while player_hand.get_value() < 21:
         ans = input('Взять карту или стоп? (h/s): ')
         if ans == 'h':
             player_hand.add_card(d.deal_card())
             print(player_hand)
-            # Если у игрока перебор - дилеру нет смысла набирать карты
             if player_hand.get_value() > 21:
                 print('Ты проиграл!')
                 in_game = False
@@ -109,17 +85,15 @@ def new_game():
             break
     print('=' * 20)
     if in_game:
-        # По правилам дилер обязан набирать карты пока его счет меньше 17
+
         while dealer_hand.get_value() < 17:
             dealer_hand.add_card(d.deal_card())
             print(dealer_hand)
-            # Если у дилера перебор играть дальше нет смысла - игрок выиграл
             if dealer_hand.get_value() > 21:
                 print('У Дилера перебор')
                 in_game = False
     if in_game:
-        # Ни у кого не было перебора - сравниваем количество очков у игрока и дилера.
-        # В нашей версии если у дилера и игрока равное количество очков - выигрывает казино
+
         if player_hand.get_value() > dealer_hand.get_value():
             print('{} - победил!'.format(player_hand))
         elif player_hand.get_value() == dealer_hand.get_value():
