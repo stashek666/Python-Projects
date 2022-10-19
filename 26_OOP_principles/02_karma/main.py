164ff82c0736d986ee3a7582576c57050918c9d9
Stas Vinnikov <stashek666@gmail.com>
2022-10-19 20:38:19 +0300

    2е задание решено

--- 26_OOP_principles/02_karma/main.py
+++ 26_OOP_principles/02_karma/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+import random
+
+
+def one_day():
+
+    exceptions = ['KillError', 'DrunkError', 'CarCrashError',
+                  'GluttonyError', 'DepressionError']
+    karma = 0
+    count = 0
+
+    with open('karma.log', 'w') as file:
+        while karma < 500:
+            count += 1
+            karma += random.randint(1, 7)
+            number = random.randint(1, 10)
+            if number == 10:
+                file.write(random.choice(exceptions) + '\n')
+
+    print('Прошло дней: ', count)
+
+
+one_day()
+
