9ee1ef812fc3c74e4e5e4db35f944edc5d338426
Stas Vinnikov <stashek666@gmail.com>
2022-10-19 20:47:34 +0300

    3е задание решено

--- 26_OOP_principles/03_my_dict/main.py
+++ 26_OOP_principles/03_my_dict/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+class MyDict(dict):
+    def get(self, key, default=0):
+        return super().get(key, default)
+
+
+new_dict = MyDict()
+new_dict['Вася'] = 20
+new_dict['Саша'] = 25
+new_dict['Степа'] = 30
+print(new_dict)
+print(new_dict.get('Вася'))
