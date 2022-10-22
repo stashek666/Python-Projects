fb2ca3ecef9d98616ff54c32dacdd64fdd48dbe6
Stas Vinnikov <stashek666@gmail.com>
2022-10-22 14:47:03 +0300

    3е задание решено

--- 27_Iterators_and_generators/03_files_path/main.py
+++ 27_Iterators_and_generators/03_files_path/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+import os
+from collections.abc import Iterable
+
+
+def gen_files_path(dir_name: str, path: str) -> Iterable[str]:
+    print('Текущий путь: ', path)
+    for i_elem in os.listdir(path):
+        current_path = os.path.join(path, i_elem)
+        if os.path.isdir(current_path) and i_elem != dir_name:
+            gen_files_path(dir_name, current_path)
+        elif os.path.isfile(os.path.join(path, i_elem)):
+            yield os.path.join(path, i_elem)
+
+
+directory = 'SkillBox'
+abs_path = os.path.abspath(os.path.join(os.path.sep))
+res = gen_files_path(dir_name=directory, path=abs_path)
+
+for i in res:
+    print(i)
