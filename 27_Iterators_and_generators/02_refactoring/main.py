9938a17bf5997090f2d5932cb39060932ef21fdd
Stas Vinnikov <stashek666@gmail.com>
2022-10-22 14:34:12 +0300

    2е задание решено

--- 27_Iterators_and_generators/02_refactoring/main.py
+++ 27_Iterators_and_generators/02_refactoring/main.py
@@ -15,3 +15,25 @@ for x in list_1:
         break
 
 # TODO провести рефакторинг кода
+from collections.abc import Iterable
+
+
+def get_num_gen(lst_1: list, lst_2: list, num: int) -> Iterable[int]:
+    for x in lst_1:
+        for y in lst_2:
+            result = x * y
+            print(x, y, result)
+            if result == num:
+                print('Found!!!')
+                yield result
+                return
+
+
+list_1 = [2, 5, 7, 10]
+list_2 = [3, 8, 4, 9]
+to_find = 56
+
+res = get_num_gen(lst_1=list_1, lst_2=list_2, num=to_find)
+
+for i in res:
+    print('result:', i)
