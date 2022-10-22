2c342223aa6bbceb52966730338eeaa095346f06
Stas Vinnikov <stashek666@gmail.com>
2022-10-22 15:11:37 +0300

    4е задание решено

--- 27_Iterators_and_generators/04_hof_seq/main.py
+++ 27_Iterators_and_generators/04_hof_seq/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+from collections.abc import Iterable
+
+
+def hofstadter_seq(start: list) -> Iterable[int]:
+    if start != [1, 1]:
+        print('End')
+        return
+    seq = start[:]
+    while 1:
+        q = seq[-seq[-1]] + seq[-seq[-2]]
+        seq.append(q)
+        yield q
+
+
+N = int(input('Сколько чисел в последовательности: '))
+counter = 1
+result = hofstadter_seq(start=[1, 1])
+
+print('Результат:')
+for i in result:
+    if counter > N:
+        break
+    print(i, end=' ')
+    counter += 1
